0e69cdacd9929414e876c95b6269b2f9f73c0211
Rachel Williams <williamsrachelada@gmail.com>
2019-02-13 05:05:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var empty = [];
+  for (var i = 0; i < musicians.length; i++) {
+    empty.push(musicians[i] + ' plays ' + instruments[i])
+  }
+  return empty;
+}
